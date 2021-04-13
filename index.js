4f6b3b3c92035a7fd4dd42bd03cd5ac0cd0343a1
morshedalam3 <morshedhasan1969@gmail.com>
2021-04-13 19:03:06 +0600

    [added]

--- index.js
+++ index.js
@@ -11,116 +11,68 @@ const port = process.env.PORT || 8000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ossg7.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log('error', err)
+  const collection = client.db("redoniondb").collection("foodify");
+  const featureCollection = client.db("redoniondb").collection("feature");
+  console.log('connected')
 
-// const collection = client.db("redoniondb").collection("foodify");
-// const orderCollection = client.db("redoniondb").collection("order");
+  app.get( '/food/:id', (req, res) => {
+    const foodId = Number(req.params.id);
+    collection.find({id:foodId})
+    .toArray((err, items) => {
+      res.send(items)
+    })
+  })
+
+  app.get( '/features',(req, res) => {
+    featureCollection.find()
+    .toArray((err, items) => {
+      res.send(items)
+    })
+  })
+
+  app.get('/foods', (req, res) => {
+    collection.find()
+    .toArray((err, items) => {
+        res.send(items)
+    })
+  })
+})
 
 app.get('/' , (req, res) => {
-  res.send("Welcome to Red Onion Backend Server");
+  res.send("Welcome to Red Onion Server");
 })
 
-
-app.get('/foods' , (req, res) => {
-  client = new MongoClient(uri ,{useNewUrlParser:true});
-  client.connect(err => {
-      if(err){
-          console.log(err);
-      }else{
-          const collection = client.db('redoniondb').collection('foodify');
-          collection.find().toArray((rej,documents) => {
-              if(rej){
-                  console.log(rej);
-                  res.status(500).send("Filed to Fetch Data ")
-              }else{
-                  res.send(documents);
-              }
-              client.close()
-          })
-      }
-  })
-})
-
-app.get('/food/:id', (req,res) => {
-  client = new MongoClient(uri,{useNewUrlParser:true,useUnifiedTopology: true})
-  const foodId = Number(req.params.id)
-
-  client.connect(err => {
-    const collection = client.db("redoniondb").collection("foodify");
-      console.log(foodId);
-      collection.find({id:foodId}).toArray((err, documents) => {
-          if(err){
-              console.log(err);
-          }else{
-              res.send(documents[0]);
-          }
-          client.close();
-      })
-  })
-})
-
-app.get('/features' , (req,res) => {
-  client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
-  client.connect(err => {
-    const collection = client.db("redoniondb").collection("foodify");
-      collection.find().toArray((rej,documents) => {
-          if(rej){
-              res.status(500).send("Failed to fetch data");
-          }else{
-              res.send(documents)
-          }
-      }) 
-      
-  })
-
-})
-
-// Post routes
-app.post('/submitorder' , (req,res) => {
-  const data = req.body;
-  console.log(data);
-  client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
-  client.connect(err => {
-    const collection = client.db("redoniondb").collection("foodify");
-      collection.insert(data , (rej, result) =>  {
-          if(rej){
-              res.status(500).send("Filed to inset")
-          }else{
-              res.send(result.ops[0])
-          }
-      })
-  })
-})
-
-// Bellows are dummy post method used just one time
 app.post('/addfood' , (req,res) => {
-  const data = req.body;
-  console.log(data);
-  client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
-  client.connect(err => {
-    const collection = client.db("redoniondb").collection("foodify");
-      collection.insertMany(data , (rej, result) =>  {
-          if(rej){
-              res.status(500).send("Filed to inset")
-          }else{
-              res.send(result)
-          }
-      })
-  })
+    const data = req.body;
+    console.log(data);
+    client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
+    client.connect(err => {
+      const collection = client.db("redoniondb").collection("foodify");
+        collection.insertMany(data , (rej, result) =>  {
+            if(rej){
+                res.status(500).send("Filed to inset")
+            }else{
+                res.send(result.ops)
+            }
+        })
+    })
 })
+
 app.post('/addfeatures' , (req,res) => {
-  const data = req.body;
-  console.log(data);
-  client = new MongoClient(uri , {useNewUrlParser:true , useUnifiedTopology: true});
-  client.connect(err => {
-    const collection = client.db("redoniondb").collection("foodify");
-      collection.insertMany(data , (rej, result) =>  {
-          if(rej){
-              res.status(500).send("Filed to inset")
-          }else{
-              res.send(result)
-          }
-      })
-  })
+    const data = req.body;
+    console.log(data);
+    client.connect(err => {
+      const featureCollection = client.db("redoniondb").collection("feature");
+        featureCollection.insertMany(data , (rej, result) =>  {
+            if(rej){
+                res.status(500).send("Filed to inset")
+            }else{
+                res.send(result.ops)
+            }
+        })
+    })
 })
 
 app.listen(port, () => {
